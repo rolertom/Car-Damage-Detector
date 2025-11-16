b9f79047e7f3ea4c79ebe1dc19b9ab2967699ef5
dominicsiagian <dominicsiagian@gmail.com>
2025-11-17 00:54:42 +0700

    update model to keras format

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-model = load_model("car_damage_model.h5")
+model = load_model("car_damage_model.keras")
 
 st.set_page_config(page_title="Car Damage Detector", layout="wide")
 
